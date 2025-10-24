8ba5c14c88eb74ae477b94e5b7f356bb064b1505
deexiths <100189970+deexiths@users.noreply.github.com>
2025-10-24 14:28:10 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -2,10 +2,3 @@ CREATE TABLE department (
     department_id SERIAL PRIMARY KEY,
     department_name VARCHAR(100) NOT NULL
 );
-
-CREATE TABLE employee (
-    employee_id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(100) NOT NULL,
-    position VARCHAR(50),
-    department_id INT REFERENCES department(department_id) ON DELETE CASCADE
-);
