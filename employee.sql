44fa4cd7f2a252645811a62f63218f91c32288d7
deexiths <100189970+deexiths@users.noreply.github.com>
2025-10-26 17:23:32 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,51 +1,20 @@
-
-CREATE TABLE members (
-    member_id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE,
-    email VARCHAR(150) NOT NULL UNIQUE
+CREATE TABLE project (
+    project_id SERIAL PRIMARY KEY,
+    project_name VARCHAR(100) NOT NULL,
+    client_name VARCHAR(100)
 );
 
-
-CREATE TABLE member_profiles (
-    profile_id SERIAL PRIMARY KEY,
-    member_id INT UNIQUE NOT NULL,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    phone_number VARCHAR(20),
-    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE books (
-    book_id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    published_year INT,
-    category VARCHAR(100)
-);
-
-CREATE TABLE authors (
-    author_id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL
-);
-
--- ===============================
--- Book-Authors Table (Many-to-Many)
--- ===============================
-CREATE TABLE book_authors (
-    book_id INT NOT NULL,
-    author_id INT NOT NULL,
-    PRIMARY KEY (book_id, author_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE loans (
-    loan_id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    book_id INT NOT NULL,
-    loan_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    return_date TIMESTAMP,
-    FOREIGN KEY (member_id) REFERENCES members(member_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
+CREATE TABLE employee_project (
+    emp_id INT NOT NULL,
+    project_id INT NOT NULL,
+    role VARCHAR(50),
+    PRIMARY KEY (emp_id, project_id),
+    CONSTRAINT fk_emp
+        FOREIGN KEY (emp_id)
+        REFERENCES employee(emp_id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_project
+        FOREIGN KEY (project_id)
+        REFERENCES project(project_id)
+        ON DELETE CASCADE
 );
